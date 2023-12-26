f82c4ea59c78e1285ec5b9dc4ccce1c70a9c25f2
osmn_turhn <osmannuri.turhan01@gmail.com>
2023-12-26 14:13:03 +0300

    Update bio in About.jsx

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -7,10 +7,16 @@ const About = () => {
         <div className="bio-text">
           <h1 className="bio-title">BIO</h1>
           <p className="bio-pha">
-            I am a Full Stack Web Developer with a background in the
-            construction industry. I have a passion for building things and
-            solving problems. I am a team player and enjoy working with others.
-            I am always looking to learn new things and improve my skills.
+            My bachelor's degree in International Trade and Logistics and my
+            ongoing master's studies in this field havegiven me a deep insight
+            into the complex world of global trade. At the same time, I have
+            been trained indifferent areas of software for the last three years,
+            with a particular focus on front-end development. I havecontinuously
+            improved myself through the projects I have realized and the
+            training I have received during thisprocess. Although I have come a
+            long way in this field, I am aware that there is still a lot to
+            learn and I amcommitted to constantly renewing and improving myself
+            on this journey.
           </p>
         </div>
       </section>
